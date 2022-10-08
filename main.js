aadb9ee44afad35f6f3ec02d9634025ec7c1574a
cvalhdez <cvalhdez@gmail.com>
2022-10-08 14:55:19 -0500

    Se realizan ajustes para vista mobile

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const menuToggle = document.querySelector(".menuToggle")
 const navMenu = document.querySelector(".navMenu")
 const gridContainer = document.querySelector(".gridContainer")
+const html = document.querySelector("html")
 menuToggle.addEventListener("click", () =>{
   navMenu.classList.toggle("navMenu_visible")
-  gridContainer.classList.toggle("noScroll")
+  gridContainer.classList.toggle("noScrollBody")
+  html.classList.toggle("noScrollHtml")
 })
